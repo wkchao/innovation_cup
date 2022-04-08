d1e3e4dea2b2d260b962eacbf35e552d84fd3a43
kude77 <412724618@qq.com>
2022-04-08 14:07:05 +0800

    123

--- func.py
+++ func.py
@@ -1,14 +1,16 @@
 import jieba
+import jieba.posseg
 import re
 
 
 def seg_depart(_text):
     stopwords = [line.strip() for line in open('./stopwords.txt', encoding='utf-8').readlines()]
     _text = re.sub(u'[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+', "", _text)
-    sentence_depart = jieba.cut(_text.strip())
+    sentence_depart = jieba.posseg.cut(_text.strip(), use_paddle=True)
     text = ''
-    for word in sentence_depart:
+    for word, flag in sentence_depart:
+
         if word not in stopwords:
-            if word != '\t':
+            if word != '\t' and flag in ['n', 's', 'ns', 'nt', 'nw', 'nz', 'PER', 'LOC', 'ORG']:
                 text = text + word + ' '
     return text if text != '' else None
